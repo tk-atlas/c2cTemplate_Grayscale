a775932aa2ded004b264bfb32c1c1851cd6c2227
Tom Kane <tkane@atlassian.com>
2019-02-25 07:17:13 +0000

    Add transformers to page using AJAX.

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,32 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
+from datetime import datetime
+from random import shuffle
+
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def main():
     return render_template("index.html")
+
+@app.route('/api/transformers.json', methods=['GET'])
+def api_transformers():
+    transformers = [
+        {"name": "Frenzy", "height": "3'", "image_url": "https://upload.wikimedia.org/wikipedia/en/6/69/Frenzy-art.jpg"},
+        {"name": "Jazz", "height": "15'", "image_url": "https://upload.wikimedia.org/wikipedia/en/3/37/Jazz-animated.jpg"},
+        {"name": "Bumblebee", "height": "16'", "image_url": "https://upload.wikimedia.org/wikipedia/en/3/37/BumblebeeHIRES.jpg"},
+        {"name": "Barricade", "height": "16'", "image_url": "https://upload.wikimedia.org/wikipedia/en/d/de/Barricade-g1toy.jpg"},
+        {"name": "Ratchet", "height": "20'", "image_url": "https://upload.wikimedia.org/wikipedia/en/3/30/Ratchet_%28Transformers_-_Generation_1%29.jpg"},
+        {"name": "Ironhide", "height": "22'", "image_url": "https://upload.wikimedia.org/wikipedia/en/5/56/Ironhide-universeclassictoy.jpg"},
+        {"name": "Optimus Prime", "height": "28'", "image_url": "https://upload.wikimedia.org/wikipedia/en/0/03/Optimusprime-armada.png"},
+        {"name": "Starscream", "height": "31'", "image_url": "https://upload.wikimedia.org/wikipedia/en/5/59/Starscream_%28Transformers%29.jpg"},
+        {"name": "Blackout", "height": "33'", "image_url": "https://upload.wikimedia.org/wikipedia/en/0/05/Blackout-energontoy.jpg"},
+        {"name": "Megatron", "height": "35'", "image_url": "https://upload.wikimedia.org/wikipedia/en/7/7f/Megatron.jpg"}
+    ]
+
+    shuffle(transformers)
+
+    return jsonify(transformers)
 
 if __name__ == '__main__':
     app.run()
--- static/js/grayscale.js
+++ static/js/grayscale.js
@@ -40,3 +40,57 @@
   $(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
+
+function getDataFromApi() {
+    var status = document.getElementById("api_status");
+    status.innerText = 'Fetching data from API...';
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/api/transformers.json');
+
+    xhr.onload = function() {
+
+        if (xhr.status === 200) {
+            var apiResponse = JSON.parse(xhr.responseText);
+            console.log(apiResponse);
+
+            var apiData = document.getElementById("api_data");
+            apiData.innerHTML = '';  // clear apiData
+
+            apiResponse.forEach(function(transformer) {
+                var card = document.createElement("div");
+                card.classList.add("card");
+                apiData.appendChild(card);
+
+                var cardBody = document.createElement("div");
+                cardBody.classList.add("card-body");
+                card.appendChild(cardBody);
+
+                var cardTitle = document.createElement("h4");
+                cardTitle.classList.add("card-title");
+                var nameElement = document.createTextNode(transformer.name);
+                cardTitle.appendChild(nameElement);
+                cardBody.appendChild(cardTitle);
+
+                var heightP = document.createElement("p");
+                heightP.classList.add("card-text");
+                var heightElement = document.createTextNode("Height: " + transformer.height);
+                heightP.appendChild(heightElement);
+                cardBody.appendChild(heightP);
+
+                var cardImage = document.createElement("img");
+                cardImage.setAttribute("src", transformer.image_url);
+                cardImage.setAttribute("alt", transformer.name);
+                cardImage.classList.add("card-img-bottom");
+                card.appendChild(cardImage);
+            });
+
+            status.innerText = '';
+        }
+        else {
+            alert('Request failed. Returned status of ' + xhr.status);
+            status.innerText = 'Error: Could not get data from API.';
+        }
+    };
+    xhr.send();
+}
